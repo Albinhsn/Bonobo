b788b79c09419e98bfe7bc024d1b112209770bcc
Albinhsn <albin.henningsson1@gmail.com>
2022-09-27 19:42:29 +0200

    90% of the work done only 90% of the time left

--- Lexer.hs
+++ Lexer.hs
@@ -2,24 +2,65 @@ module Lexer where
 
 import Token
 
-data Lexer = Lexer {input :: !String, position :: !Int, readPosition :: !Int, ch :: !Char} deriving (Show)
+data Lexer = Lexer {input :: !String, position :: !Int, readPosition :: !Int, tokens :: ![Token]} deriving (Show)
 
---parseStringToToken :: String -> [Token]
---parseStringToToken s = [getToken n | n <- s]
+isValidSpecialChar :: Char -> Bool
+isValidSpecialChar ch = ch == '=' || ch == '+' || ch == ',' || ch == ';' || ch == '(' || ch == ')' || ch == '{' || ch == '}'
 
-charToString :: Char -> String
-charToString c = [c]
+isInvalid :: Char -> Bool
+isInvalid ch = True
 
-createLexer :: String -> Lexer
-createLexer s = Lexer {input = s, position = 0, readPosition = 1, ch = head s}
+getCurrentChar :: Lexer -> [Char]
+getCurrentChar lexer = [input lexer !! position lexer]
 
-getToken :: (Lexer, Char) -> Token
-getToken (lexer, s) = token
+getToken :: Lexer -> Lexer
+getToken lexer = l
   where
-    token
-      | s == '=' || s == '+' || s == ',' || s == ';' || s == '(' || s == ')' || s == '{' || s == '}' = Token {typ = ASSIGN, literal = charToString s}
-      | isLetter s = Token {typ = IDENT, literal = readIdentifier lexer}
-      | otherwise = Token {typ = EOF, literal = "EOF"}
+    l
+      | length (input lexer) < readPosition lexer = lexer
+      | isValidSpecialChar (head (getCurrentChar lexer)) =
+        getToken
+          Lexer
+            { input = input lexer,
+              position = readPosition lexer,
+              readPosition = readPosition lexer + 1,
+              tokens =
+                tokens lexer
+                  ++ [ Token
+                         { typ = ASSIGN,
+                           literal = getCurrentChar lexer
+                         }
+                     ]
+            }
+      | isLetter (head (getCurrentChar lexer)) =
+        getToken
+          ( readIdentifier
+              Lexer
+                { input = input lexer,
+                  position = readPosition lexer,
+                  readPosition = readPosition lexer + 1,
+                  tokens =
+                    tokens lexer
+                      ++ validateIdentifier
+                        Token
+                          { typ = IDENT,
+                            literal = getCurrentChar lexer
+                          }
+                }
+          )
+      | otherwise =
+        Lexer
+          { input = input lexer,
+            position = readPosition lexer,
+            readPosition = readPosition lexer + 1,
+            tokens =
+              tokens lexer
+                ++ [ Token
+                       { typ = EOF,
+                         literal = "EOF"
+                       }
+                   ]
+          }
 
 isLetter :: Char -> Bool
 isLetter c = b
@@ -28,25 +69,56 @@ isLetter c = b
       | 'a' <= c && c >= 'z' || 'A' <= c && c >= 'Z' || c == '_' = True
       | otherwise = False
 
-nextToken :: Lexer -> Token
-nextToken lexer = t
+readIdentifier :: Lexer -> Lexer
+readIdentifier lexer = l
   where
-    t
-      | isLetter (ch lexer) = getToken (lexer, ch lexer)
-      | otherwise = Token {typ = ILLEGAL, literal = "ILLEGAL"}
-
-readChar :: Lexer -> Lexer
-readChar pl = nl
-  where
-    nl
-      | readPosition pl < length (input pl) =
+    l
+      | readPosition lexer > length (input lexer) = lexer
+      | isLetter (input lexer !! position lexer) =
+        readIdentifier
+          Lexer
+            { input = input lexer,
+              position = readPosition lexer,
+              readPosition = readPosition lexer + 1,
+              tokens =
+                [ Token
+                    { typ = IDENT,
+                      literal = literal (getLastToken lexer) ++ getCurrentChar lexer
+                    }
+                ]
+            }
+      | otherwise =
         Lexer
-          { input = input pl,
-            position = readPosition pl,
-            readPosition = readPosition pl + 1,
-            ch = input pl !! readPosition pl
+          { input = input lexer,
+            position = position lexer,
+            readPosition = readPosition lexer,
+            tokens = pop (tokens lexer) ++ validateIdentifier (head (tokens lexer))
           }
-      | otherwise = Lexer {input = input pl, position = readPosition pl, readPosition = readPosition pl + 1, ch = '\r'}
 
-readIdentifier :: Lexer -> String
-readIdentifier lexer = " "
+getLastToken :: Lexer -> Token
+getLastToken l = tokens l !! (length (tokens l) - 1)
+
+validateIdentifier :: Token -> [Token]
+validateIdentifier t = [t]
+
+pop :: [a] -> [a]
+pop [] = []
+pop a = init a
+
+charToString :: Char -> String
+charToString c = [c]
+
+createLexer :: String -> Lexer
+createLexer s = Lexer {input = s, position = 0, readPosition = 1, tokens = []}
+
+-- identifier :: Lexer -> Token
+-- identifier lexer = t
+--   where
+--     t | isLetter (input!!(readPosition lexer) lexer) =
+
+-- nextToken :: Lexer -> Token
+-- nextToken lexer = t
+--   where
+--     t
+--       | isLetter (ch lexer) = getToken (lexer, ch lexer)
+--       | otherwise = Token {typ = ILLEGAL, literal = "ILLEGAL"}
--- Main.hs
+++ Main.hs
@@ -3,11 +3,7 @@ module Main where
 import Lexer
 
 main = do
-  let l = createLexer ";.=()"
-  print l
-  let l2 = readChar l
-  let len = length (input l)
-  let readPos = readPosition l
-  print len
-  print readPos
-  print l2
+  -- let l = createLexer ";,=()"
+  let l = createLexer "albin;,=()"
+  let s = getToken l
+  print s
