40f89727bc6ca4d43bfa8bb9a67b7016ffec8665
Albinhsn <albin.henningsson1@gmail.com>
2022-09-28 19:17:59 +0200

    Actual lexer?

--- Lexer.hs
+++ Lexer.hs
@@ -7,6 +7,9 @@ data Lexer = Lexer {input :: !String, position :: !Int, readPosition :: !Int, to
 isValidSpecialChar :: Char -> Bool
 isValidSpecialChar ch = ch == '=' || ch == '+' || ch == ',' || ch == ';' || ch == '(' || ch == ')' || ch == '{' || ch == '}'
 
+isEmptyChar :: Char -> Bool
+isEmptyChar ch = ch == ' ' || ch == '\n' || ch == '\t'
+
 isInvalid :: Char -> Bool
 isInvalid ch = True
 
@@ -18,6 +21,14 @@ getToken lexer = l
   where
     l
       | length (input lexer) < readPosition lexer = lexer
+      | isEmptyChar (head (getCurrentChar lexer)) =
+        getToken
+          Lexer
+            { input = input lexer,
+              position = readPosition lexer,
+              readPosition = readPosition lexer + 1,
+              tokens = tokens lexer
+            }
       | isValidSpecialChar (head (getCurrentChar lexer)) =
         getToken
           Lexer
@@ -99,7 +110,13 @@ getLastToken :: Lexer -> Token
 getLastToken l = tokens l !! (length (tokens l) - 1)
 
 validateIdentifier :: Token -> [Token]
-validateIdentifier t = [t]
+validateIdentifier t = token
+  where
+    token
+      | literal t == "int" = [Token {typ = INT, literal = "int"}]
+      | literal t == "function" = [Token {typ = FUNCTION, literal = "functiofunctionn"}]
+      | literal t == "let" = [Token {typ = LET, literal = "let"}]
+      | otherwise = [Token {typ = IDENT, literal = literal t}]
 
 pop :: [a] -> [a]
 pop [] = []
@@ -110,15 +127,3 @@ charToString c = [c]
 
 createLexer :: String -> Lexer
 createLexer s = Lexer {input = s, position = 0, readPosition = 1, tokens = []}
-
--- identifier :: Lexer -> Token
--- identifier lexer = t
---   where
---     t | isLetter (input!!(readPosition lexer) lexer) =
-
--- nextToken :: Lexer -> Token
--- nextToken lexer = t
---   where
---     t
---       | isLetter (ch lexer) = getToken (lexer, ch lexer)
---       | otherwise = Token {typ = ILLEGAL, literal = "ILLEGAL"}
--- Main.hs
+++ Main.hs
@@ -4,6 +4,6 @@ import Lexer
 
 main = do
   -- let l = createLexer ";,=()"
-  let l = createLexer "albin;,=()"
+  let l = createLexer "albin ; ,=()"
   let s = getToken l
   print s
