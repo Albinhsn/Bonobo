dd4c5c5204bc383e36018677d68423ae92e11a4b
albinhsn <albin.henningsson1@gmail.com>
2023-09-21 10:10:13 +0200

    test logical

--- test/TestAst.cpp
+++ test/TestAst.cpp
@@ -21,31 +21,6 @@ TEST(TestIntVariable, TestIntVar) {
         "5");
 }
 
-TEST(TestLogicalOp, TestAnd) {
-    std::string source = "var a: int = 5 and 3;";
-
-    std::vector<Stmt *> result = compile(source.c_str());
-
-    VarStmt *varStmt = (VarStmt *)result[0];
-    EXPECT_EQ(varStmt->initializer->type, LOGICAL_EXPR);
-
-    LogicalExpr *expr = (LogicalExpr *)varStmt->initializer;
-    EXPECT_EQ(expr->op, AND_LOGICAL);
-    EXPECT_EQ(expr->left->type, LITERAL_EXPR);
-    EXPECT_EQ(expr->right->type, LITERAL_EXPR);
-}
-
-TEST(TestLogicalOp, TestOr) {
-    std::string source = "var a: int = 5 or 3;";
-
-    std::vector<Stmt *> result = compile(source.c_str());
-
-    VarStmt *varStmt = (VarStmt *)result[0];
-    EXPECT_EQ(varStmt->initializer->type, LOGICAL_EXPR);
-
-    LogicalExpr *expr = (LogicalExpr *)varStmt->initializer;
-    EXPECT_EQ(expr->op, OR_LOGICAL);
-}
 
 TEST(TestUnaryOp, TestBang) {
     std::string source = "var a: int = !5;";
