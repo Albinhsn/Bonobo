324f6cb422a20407c892485a638a972c703efed9
albinhsn <albin.henningsson1@gmail.com>
2023-09-25 17:10:39 +0200

    embedd llvm

--- test/TestBinary.cpp
+++ test/TestBinary.cpp
@@ -1,10 +1,24 @@
+
 #include "../src/common.h"
 #include "../src/compiler.h"
+#include "../src/llvm.h"
 #include "../src/stmt.h"
 #include <gtest/gtest.h>
 
+static std::string readFile(const char *path) {
+    std::ifstream t(path);
+    std::stringstream buffer;
+    if (t.fail()) {
+        std::cout << "file doesn't exist\n";
+        exit(1);
+    }
+    buffer << t.rdbuf();
+    t.close();
+    return buffer.str();
+}
+
 TEST(TestBinaryOp, TestAddOp) {
-    std::string source = "var a: int = 5 + 3;";
+    std::string source = "var a: int = 5 + 3; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -25,10 +39,15 @@ TEST(TestBinaryOp, TestAddOp) {
     EXPECT_EQ(right->literalType, INT_LITERAL);
     EXPECT_EQ(right->literal.lexeme, "3");
 
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "8");
 }
 
 TEST(TestBinaryOp, TestDivOp) {
-    std::string source = "var a: int = 5 / 3;";
+    std::string source = "var a: int = 5 / 3; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -37,10 +56,16 @@ TEST(TestBinaryOp, TestDivOp) {
 
     BinaryExpr *binaryExpr = (BinaryExpr *)varStmt->initializer;
     EXPECT_EQ(binaryExpr->op, DIV);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "1");
 }
 
 TEST(TestBinaryOp, TestMulOp) {
-    std::string source = "var a: int = 5 * 3;";
+    std::string source = "var a: int = 5 * 3; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -49,10 +74,16 @@ TEST(TestBinaryOp, TestMulOp) {
 
     BinaryExpr *binaryExpr = (BinaryExpr *)varStmt->initializer;
     EXPECT_EQ(binaryExpr->op, MUL);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "15");
 }
 
 TEST(TestBinaryOp, TestPrecedenceAddAdd) {
-    std::string source = "var a: int = 5 + 3 + 2;";
+    std::string source = "var a: int = 5 + 3 + 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -68,10 +99,16 @@ TEST(TestBinaryOp, TestPrecedenceAddAdd) {
     EXPECT_EQ(left->op, ADD);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "10");
 }
 
 TEST(TestBinaryOp, TestPrecedenceAddSub) {
-    std::string source = "var a: int = 5 + 3 - 2;";
+    std::string source = "var a: int = 5 + 3 - 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -87,10 +124,16 @@ TEST(TestBinaryOp, TestPrecedenceAddSub) {
     EXPECT_EQ(left->op, ADD);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "6");
 }
 
 TEST(TestBinaryOp, TestPrecedenceAddMul) {
-    std::string source = "var a: int = 5 + 3 * 2;";
+    std::string source = "var a: int = 5 + 3 * 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -106,10 +149,16 @@ TEST(TestBinaryOp, TestPrecedenceAddMul) {
     EXPECT_EQ(right->op, MUL);
     EXPECT_EQ(right->left->type, LITERAL_EXPR);
     EXPECT_EQ(right->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "11");
 }
 
 TEST(TestBinaryOp, TestPrecedenceAddDiv) {
-    std::string source = "var a: int = 5 + 3 / 2;";
+    std::string source = "var a: int = 5 + 3 / 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -125,10 +174,16 @@ TEST(TestBinaryOp, TestPrecedenceAddDiv) {
     EXPECT_EQ(right->op, DIV);
     EXPECT_EQ(right->left->type, LITERAL_EXPR);
     EXPECT_EQ(right->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "6");
 }
 
 TEST(TestBinaryOp, TestPrecedenceSubAdd) {
-    std::string source = "var a: int = 5 - 3 + 2;";
+    std::string source = "var a: int = 5 - 3 + 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -144,10 +199,16 @@ TEST(TestBinaryOp, TestPrecedenceSubAdd) {
     EXPECT_EQ(left->op, SUB);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "4");
 }
 
 TEST(TestBinaryOp, TestPrecedenceSubSub) {
-    std::string source = "var a: int = 5 - 3 - 2;";
+    std::string source = "var a: int = 5 - 3 - 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -163,10 +224,16 @@ TEST(TestBinaryOp, TestPrecedenceSubSub) {
     EXPECT_EQ(left->op, SUB);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "0");
 }
 
 TEST(TestBinaryOp, TestPrecedenceSubMul) {
-    std::string source = "var a: int = 5 - 3 * 2;";
+    std::string source = "var a: int = 5 - 3 * 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -182,10 +249,16 @@ TEST(TestBinaryOp, TestPrecedenceSubMul) {
     EXPECT_EQ(right->op, MUL);
     EXPECT_EQ(right->left->type, LITERAL_EXPR);
     EXPECT_EQ(right->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "1");
 }
 
 TEST(TestBinaryOp, TestPrecedenceSubDiv) {
-    std::string source = "var a: int = 5 - 3 / 2;";
+    std::string source = "var a: int = 5 - 3 / 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -201,10 +274,16 @@ TEST(TestBinaryOp, TestPrecedenceSubDiv) {
     EXPECT_EQ(right->op, DIV);
     EXPECT_EQ(right->left->type, LITERAL_EXPR);
     EXPECT_EQ(right->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "4");
 }
 
 TEST(TestBinaryOp, TestPrecedenceMulAdd) {
-    std::string source = "var a: int = 5 * 3 + 2;";
+    std::string source = "var a: int = 5 * 3 + 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -220,10 +299,16 @@ TEST(TestBinaryOp, TestPrecedenceMulAdd) {
     EXPECT_EQ(left->op, MUL);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "17");
 }
 
 TEST(TestBinaryOp, TestPrecedenceMulSub) {
-    std::string source = "var a: int = 5 * 3 - 2;";
+    std::string source = "var a: int = 5 * 3 - 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -239,10 +324,16 @@ TEST(TestBinaryOp, TestPrecedenceMulSub) {
     EXPECT_EQ(left->op, MUL);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "13");
 }
 
 TEST(TestBinaryOp, TestPrecedenceMulMul) {
-    std::string source = "var a: int = 5 * 3 * 2;";
+    std::string source = "var a: int = 5 * 3 * 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -261,10 +352,16 @@ TEST(TestBinaryOp, TestPrecedenceMulMul) {
 
     LiteralExpr *right = (LiteralExpr *)binaryExpr->right;
     EXPECT_EQ(right->literal.lexeme, "2");
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "30");
 }
 
 TEST(TestBinaryOp, TestPrecedenceMulDiv) {
-    std::string source = "var a: int = 5 * 3 / 2;";
+    std::string source = "var a: int = 5 * 3 / 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -283,10 +380,16 @@ TEST(TestBinaryOp, TestPrecedenceMulDiv) {
 
     LiteralExpr *right = (LiteralExpr *)binaryExpr->right;
     EXPECT_EQ(right->literal.lexeme, "2");
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "7");
 }
 
 TEST(TestBinaryOp, TestPrecedenceDivAdd) {
-    std::string source = "var a: int = 5 / 3 + 2;";
+    std::string source = "var a: int = 5 / 3 + 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -302,10 +405,16 @@ TEST(TestBinaryOp, TestPrecedenceDivAdd) {
     EXPECT_EQ(left->op, DIV);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "3");
 }
 
 TEST(TestBinaryOp, TestPrecedenceDivSub) {
-    std::string source = "var a: int = 5 / 3 - 2;";
+    std::string source = "var a: int = 5 / 3 - 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -321,10 +430,16 @@ TEST(TestBinaryOp, TestPrecedenceDivSub) {
     EXPECT_EQ(left->op, DIV);
     EXPECT_EQ(left->left->type, LITERAL_EXPR);
     EXPECT_EQ(left->right->type, LITERAL_EXPR);
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "-1");
 }
 
 TEST(TestBinaryOp, TestPrecedenceDivMul) {
-    std::string source = "var a: int = 5 / 3 * 2;";
+    std::string source = "var a: int = 5 / 3 * 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -343,10 +458,16 @@ TEST(TestBinaryOp, TestPrecedenceDivMul) {
 
     LiteralExpr *right = (LiteralExpr *)binaryExpr->right;
     EXPECT_EQ(right->literal.lexeme, "2");
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "2");
 }
 
 TEST(TestBinaryOp, TestPrecedenceDivDiv) {
-    std::string source = "var a: int = 5 / 3 / 2;";
+    std::string source = "var a: int = 5 / 3 / 2; printf(\"%d\", a);";
 
     std::vector<Stmt *> result = compile(source.c_str());
 
@@ -365,6 +486,12 @@ TEST(TestBinaryOp, TestPrecedenceDivDiv) {
 
     LiteralExpr *right = (LiteralExpr *)binaryExpr->right;
     EXPECT_EQ(right->literal.lexeme, "2");
+
+    LLVMCompiler *llvmCompiler = new LLVMCompiler(result);
+    llvmCompiler->compile();
+    system("lli out.ll > result.txt");
+    std::string resultTxt = readFile("result.txt");
+    EXPECT_EQ(resultTxt, "0");
 }
 
 TEST(TestBinaryOp, TestBinaryOpVar) {
